330dcac89e7dfba7efc61766355fc7d305a1633a
loowiz <loowiz@envs.net>
2026-06-07 11:33:23 +0100

    feat: Automatically setup livekit for MatrixRTC calling

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,3 +9,14 @@ if [[ "${registration:-}" -eq 1 ]]; then
 else
     registration="false"
 fi
+
+get_lk_node_ip() {
+    local public_ip4
+    public_ip4="$(curl -s https://ip.yunohost.org)" || true
+
+    lk_node_ip=""
+    if [ -n "$public_ip4" ] && ynh_validate_ip --family=4 --ip_address="$public_ip4"
+    then
+        lk_node_ip="--node-ip $public_ip4"
+    fi
+}
